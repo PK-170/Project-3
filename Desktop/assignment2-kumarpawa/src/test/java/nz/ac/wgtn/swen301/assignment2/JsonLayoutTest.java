164f3bbb94c8009d598a87016ce7cd9171c8d7f9
kumarpawa <kumarpawa@myvuw.ac.nz>
2022-05-06 23:27:17 +1200

    tests cases

--- Desktop/assignment2-kumarpawa/src/test/java/nz/ac/wgtn/swen301/assignment2/JsonLayoutTest.java
+++ Desktop/assignment2-kumarpawa/src/test/java/nz/ac/wgtn/swen301/assignment2/JsonLayoutTest.java
@@ -22,4 +22,44 @@ public class JsonLayoutTest {
      System.out.println(string);
  }
 
+ @Test
+ public void test01(){
+     JSONObject jsonObject = new JSONObject("{level:FATAL,logger:nz.ac.wgtn.swen301.assignment2.JSONLayout,start time:1619145259278, thread:main, message:This is the Fatal Level}");
+     assertNotNull(jsonObject);
+     System.out.println(jsonObject.get("level"));
+     assertEquals("FATAL",jsonObject.get("level"));
+
+ }
+
+ @Test
+ public void test02(){
+     JSONObject jsonObject = new JSONObject("{level:WARN,logger:nz.ac.wgtn.swen301.assignment2.JSONLayout,start time:1619145259278, thread:main, message:This is the Warn Level}");
+     assertNotNull(jsonObject);
+     System.out.println(jsonObject.get("start time"));
+ }
+
+ @Test
+ public void test03(){
+     JSONObject jsonObject = new JSONObject("{level:ERROR,logger:nz.ac.wgtn.swen301.assignment2.JSONLayout,start time:1619145259278, thread:main, message:This is the Error Level}");
+     assertNotNull(jsonObject);
+     System.out.println(jsonObject.get("logger"));
+     assertEquals("nz.ac.wgtn.swen301.assignment2.JSONLayout", jsonObject.get("logger"));
+ }
+
+ @Test
+ public void test04(){
+     JSONObject jsonObject = new JSONObject("{level:FATAL,logger:nz.ac.wgtn.swen301.assignment2.JSONLayout,start time:1619145259278, thread:main, message:This is the Info Level}");
+     assertNotNull(jsonObject);
+     System.out.println(jsonObject.get("thread"));
+     assertEquals("main", jsonObject.get("thread"));
+ }
+
+ @Test
+ public void test05(){
+     JSONObject jsonObject = new JSONObject("{level:FATAL,logger:nz.ac.wgtn.swen301.assignment2.JSONLayout,start time:1619145259278, thread:main, message:This is the FATAL Level}");
+     assertNotNull(jsonObject);
+     System.out.println(jsonObject.get("message"));
+     assertEquals("This is the FATAL Level", jsonObject.get("message"));
+ }
+
 }
